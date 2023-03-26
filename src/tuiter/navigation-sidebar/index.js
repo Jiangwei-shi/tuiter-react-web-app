04bd030da4e275c34779cf344dfacc61ba5b88d9
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2023-03-26 19:37:12 -0400

    finished Tuiter part

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {useLocation} from "react-router";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faHome,
+  faHashtag,
+  faBell,
+  faEnvelope,
+  faBookmark,
+  faList,
+  faUser,
+  faEllipsisH,
+  faTag
+} from "@fortawesome/free-solid-svg-icons";
 
 const NavigationSidebar = () => {
   const {pathname} = useLocation();
@@ -10,34 +22,65 @@ const NavigationSidebar = () => {
     <div className="list-group">
       <a className="list-group-item">Tuiter</a>
       <Link to="/tuiter/home" className={`list-group-item ${active === 'home'?'active':''}`}>
-        Home
+        <div className="row">
+          <div className="col-1"><FontAwesomeIcon icon={faHome} size={"lg"}/></div>
+          <div className="d-done d-sm-none d-md-none d-lg-none d-xl-block col ms-3">Home</div>
+        </div>
       </Link>
       <Link to="/tuiter/explore" className={`list-group-item ${active === 'explore'?'active':''}`}>
-        Explore
+        <div className="row">
+          <div className="col-1"><FontAwesomeIcon icon={faHashtag} size={"lg"}/></div>
+          <div className="d-done d-sm-none d-md-none d-lg-none d-xl-block col ms-3">Explore</div>
+        </div>
       </Link>
       <a className={`list-group-item
+                    ${active === 'lab' ? 'active' : ''}`}>
+        <div className="row">
+          <div className="col-1"><FontAwesomeIcon icon={faTag} size={"lg"}/></div>
+          <div className="d-done d-sm-none d-md-none d-lg-none d-xl-block col ms-3">Labs</div>
+        </div>
+      </a>
+      <a className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
-        Notifications
+        <div className="row">
+          <div className="col-1"><FontAwesomeIcon icon={faBell} size={"lg"}/></div>
+          <div className="d-done d-sm-none d-md-none d-lg-none d-xl-block col ms-3">Notifications</div>
+        </div>
       </a>
       <a className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
-        Messages
+        <div className="row">
+          <div className="col-1"><FontAwesomeIcon icon={faEnvelope} size={"lg"}/></div>
+          <div className="d-done d-sm-none d-md-none d-lg-none d-xl-block col ms-3">Messages</div>
+        </div>
       </a>
       <a className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
-        Bookmarks
+        <div className="row">
+          <div className="col-1"><FontAwesomeIcon icon={faBookmark} size={"lg"}/></div>
+          <div className="d-done d-sm-none d-md-none d-lg-none d-xl-block col ms-3">Bookmarks</div>
+        </div>
       </a>
       <a className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
-        Lists
+        <div className="row">
+          <div className="col-1"><FontAwesomeIcon icon={faList} size={"lg"}/></div>
+          <div className="d-done d-sm-none d-md-none d-lg-none d-xl-block col ms-3">Lists</div>
+        </div>
       </a>
       <a className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
-        Profile
+        <div className="row">
+          <div className="col-1"><FontAwesomeIcon icon={faUser} size={"lg"}/></div>
+          <div className="d-done d-sm-none d-md-none d-lg-none d-xl-block col ms-3">Profile</div>
+        </div>
       </a>
       <a className={`list-group-item
                     ${active === 'more'?'active':''}`}>
-        More
+        <div className="row">
+          <div className="col-1"><FontAwesomeIcon icon={faEllipsisH} size={"lg"}/></div>
+          <div className="d-done d-sm-none d-md-none d-lg-none d-xl-block col ms-3">More</div>
+        </div>
       </a>
     </div>
   );
--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,10 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 import tuits from './tuits.json';
 
+const Tuits = {
+  "userName": "SpaceX",
+  "handle": "@spacex",
+  "image": "spacex.png",
+  "topic": "Space",
+  "time": "2h",
+  "liked": true,
+  "replies": 123,
+  "retuits": 234,
+  "likes": 456,
+}
+
+
 const tuitsSlice = createSlice({
   name: 'tuits',
-  initialState: tuits
+  initialState: tuits,
+  reducers: {
+    updateLikes: (state, action) => {
+      state.tuits = action.payload;
+    },
+    createTuit(state, action) {
+      state.unshift({
+        ...action.payload,
+        ...Tuits,
+        _id: (new Date()).getTime(),
+      })
+    },
+    deleteTuit(state, action) {
+      const index = state
+      .findIndex(tuit =>
+        tuit._id === action.payload);
+      state.splice(index, 1);
+    },
+  }
 });
 
 export default tuitsSlice.reducer;
+export const {updateLikes, createTuit, deleteTuit} = tuitsSlice.actions;
 
